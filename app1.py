f84b538448b34826e7d43de6e57e1de656f7f5c0
s21256 <80453511+s21256@users.noreply.github.com>
2022-04-07 11:16:17 +0200

    Add files via upload

--- app1.py
+++ app1.py
@@ -32,7 +32,7 @@ def main():
 		embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func= lambda x : embarked_d[x] )
 		pclass_radio = st.radio("Klasa", list(pclass_d.keys()), index=1, format_func=lambda x : pclass_d[x] )
 	with right:
-		age_slider = st.slider("Wiek", value=1, min_value=1, max_value=100)
+		age_slider = st.slider("Wiek", value=1, min_value=1, max_value=71)
 		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
 		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
 		fare_slider = st.slider("Cena biletu", min_value=0, max_value=94, step=1)
